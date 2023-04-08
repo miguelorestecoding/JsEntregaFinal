301cf5c4d6c67ddd19eb37a127195e8696880fd3
miguelorestecoding <miguelorestecoding@gmail.com>
2023-04-08 13:12:20 -0300

    Instalo extension de Chrome Allow Cors luego de consulta a CoderAsk. Cambio mi codigo x codigo de Coder. Falta revisar. Traer solamente la ultima cotizacion.

--- Pruebas/script.js
+++ Pruebas/script.js
@@ -1,4 +1,5 @@
-fetch("https://api.estadisticasbcra.com/usd_of", {
+// MI CODIGO
+/*fetch("https://api.estadisticasbcra.com/usd_of", {
   headers: {
     Authorization:
       "BEARER eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTI0OTYyMzQsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJtaWd1ZWxvcmVzdGVjb2RpbmdAZ21haWwuY29tIn0.JlHmFQCNxU4CVo83UiAzz0DfeEF-wLrA3Pup2N2j14fHIx_41q1I4DtUmdAXBFFa4LILJQpW87L7kgDY4YEKzQ",
@@ -9,4 +10,29 @@ fetch("https://api.estadisticasbcra.com/usd_of", {
   })
   .then((data) => {
     console.log(data);
+  });
+*/
+
+//CODIGO TUTOR
+const url = "https://api.estadisticasbcra.com/usd";
+const token =
+  "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTI0OTYyMzQsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJtaWd1ZWxvcmVzdGVjb2RpbmdAZ21haWwuY29tIn0.JlHmFQCNxU4CVo83UiAzz0DfeEF-wLrA3Pup2N2j14fHIx_41q1I4DtUmdAXBFFa4LILJQpW87L7kgDY4YEKzQ";
+
+fetch(url, {
+  headers: {
+    Authorization: `BEARER ${token}`,
+  },
+})
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Error en la solicitud: " + response.statusText);
+    }
+  })
+  .then((data) => {
+    console.log("Cotizacion dolar Banco Nacion:", data);
+  })
+  .catch((error) => {
+    console.error(error);
   });
